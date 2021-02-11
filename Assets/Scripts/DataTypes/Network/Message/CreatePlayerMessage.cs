9dc05f28232ce3b3da3d5b4eeba4978d137075ab
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-02-11 21:54:56 +0100

    Added custom network manager

---
+++ Assets/Scripts/DataTypes/Network/Message/CreatePlayerMessage.cs
@@ -0,0 +1,9 @@
+﻿using Mirror;
+
+namespace TDGame.Network.Message
+{
+    public struct CreatePlayerMessage : NetworkMessage
+    {
+        public string Name;
+    }
+}
---
+++ Assets/Scripts/Network/TDGameNetworkManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using Mirror;
+using TDGame.Network.Message;
+
+/*
+	Documentation: https://mirror-networking.com/docs/Components/NetworkManager.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkManager.html
+*/
+
+namespace TDGame.Network
+{
+    public class TDGameNetworkManager : NetworkManager
+    {
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            NetworkServer.RegisterHandler<CreatePlayerMessage>(OnCreatePlayer);
+        }
+
+        /// <summary>
+        /// Called on the client when connected to a server.
+        /// <para>The default implementation of this function sets the client as ready and adds a player. Override the function to dictate what happens when the client connects.</para>
+        /// </summary>
+        /// <param name="conn">Connection to the server.</param>
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            base.OnClientConnect(conn);
+
+            CreatePlayerMessage message = new CreatePlayerMessage { Name = "Player " + Random.Range(0, 10000) };
+
+            conn.Send<CreatePlayerMessage>(message);
+        }
+
+        void OnCreatePlayer(NetworkConnection conn, CreatePlayerMessage message)
+        {
+            GameObject gameobject = Instantiate(playerPrefab);
+
+            Debug.Log(message.Name);
+            NetworkServer.AddPlayerForConnection(conn, gameobject);
+        }
+    }
+}
