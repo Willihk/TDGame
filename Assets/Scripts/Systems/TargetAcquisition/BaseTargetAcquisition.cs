c32d53d6b68ed1e02a12a5b75d0cf080014b329e
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-02-17 15:32:35 +0100

    Added basic target acquisition

---
+++ Assets/Scripts/Systems/TargetAcquisition/BaseTargetAcquisition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TDGame.Systems.Target.Data;
+using UnityEngine;
+
+namespace TDGame.Systems.TargetAcquisition
+{
+    public abstract class BaseTargetAcquisition : MonoBehaviour
+    {
+        [SerializeField]
+        protected TargetList targetList;
+        
+        public abstract IEnumerable<GameObject> GetAvailableTargets();
+    }
+}
---
+++ Assets/Scripts/Systems/TargetAcquisition/Implementations/RangeAcquisition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TDGame.Systems.TargetAcquisition.Implementations
+{
+    public class RangeAcquisition : BaseTargetAcquisition
+    {
+        [SerializeField]
+        protected float range;
+
+        public override IEnumerable<GameObject> GetAvailableTargets()
+        {
+            var localPosition = transform.position;
+            return targetList.GetTargets().Where(x => Vector3.Distance(x.transform.position, localPosition) <= range);
+        }
+    }
+}
