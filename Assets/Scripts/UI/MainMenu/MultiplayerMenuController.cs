bf681c4317dc9b605e0cd7f2e51a7c4ce3b206b8
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-02-17 18:30:15 +0100

    Added name field

--- Assets/Scripts/UI/MainMenu/MultiplayerMenuController.cs
+++ Assets/Scripts/UI/MainMenu/MultiplayerMenuController.cs
@@ -1,24 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TDGame.Network;
 using Mirror;
+using TMPro;
+using UnityEngine;
 
-namespace TDGame
+namespace TDGame.UI.MainMenu
 {
     public class MultiplayerMenuController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
+        [SerializeField]
+        private TextMeshProUGUI nameText;
         
-        }
-
         public void StartServer()
         {
             NetworkManager.singleton.StartServer();
