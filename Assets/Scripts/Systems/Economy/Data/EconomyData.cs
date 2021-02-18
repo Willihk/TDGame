e0d50847bdd0cff0a363fd13ad49b943c4713973
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-02-18 23:01:57 +0100

    Added synced economy

---
+++ Assets/Scripts/Systems/Economy/Data/EconomyData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TDGame.Events.Base;
+using UnityEngine;
+
+namespace TDGame.Systems.Economy.Data
+{
+    [CreateAssetMenu(fileName = "EconomyData", menuName = "Data/Economy/EconomyData", order = 0)]
+    public class EconomyData : ScriptableObject
+    {
+        [SerializeField]
+        private int startCurrency = 100;
+        
+        [SerializeField]
+        private int currency;
+        public int Currency => currency;
+
+        [SerializeField]
+        private GameEvent<int> economyChanged;
+
+
+        public void ResetEconomy()
+        {
+            currency = startCurrency;
+        }
+        
+        public void SetCurrency(int amount)
+        {
+            currency = amount;
+        }
+
+        public void AddCurrency(int amount)
+        {
+            currency += amount;
+        }
+
+        public void ReduceCurrency(int amount)
+        {
+            currency = Math.Max(currency -= amount, 0);
+        }
+
+        public bool CanAfford(int amount)
+        {
+            return Currency >= amount;
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/Economy/NetworkedPlayerEconomy.cs
@@ -0,0 +1,49 @@
+﻿using Mirror;
+using TDGame.Systems.Economy.Data;
+using UnityEngine;
+
+namespace TDGame.Systems.Economy
+{
+    public class NetworkedPlayerEconomy : NetworkBehaviour
+    {
+        [SerializeField]
+        private EconomyData localEconomy;
+
+        [SerializeField]
+        [SyncVar(hook = nameof(UpdateCurrency))]
+        private int syncedCurrency;
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            localEconomy.ResetEconomy();
+        }
+
+        [Server]
+        public void ReduceCurrency(int amount)
+        {
+            localEconomy.ReduceCurrency(amount);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount">The amount to purchase for</param>
+        /// <returns>True if the purchase went through</returns>
+        [Server]
+        public bool Purchase(int amount)
+        {
+            if (!localEconomy.CanAfford(amount))
+                return false;
+
+            ReduceCurrency(amount);
+            return true;
+        }
+
+        void UpdateCurrency(int oldCurrency, int newCurrency)
+        {
+            if (isLocalPlayer)
+                localEconomy.SetCurrency(newCurrency);
+        }
+    }
+}
