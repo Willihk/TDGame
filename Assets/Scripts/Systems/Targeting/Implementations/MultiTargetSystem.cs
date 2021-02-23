a3b0ed366be2ae2b7d848a94195cfd6b66d714c4
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-02-23 20:08:00 +0100

    Added MultiTargetSystem

---
+++ Assets/Scripts/Systems/Targeting/Implementations/MultiTargetSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TDGame.Systems.Targeting.Base;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace TDGame.Systems.Targeting.Implementations
+{
+    public class MultiTargetSystem : BaseTargetingSystem
+    {
+        [SerializeField]
+        protected int maxTargets = 4;
+        
+        public IEnumerable<GameObject> GetTargets()
+        {
+            var localPosition = transform.position;
+
+            var availableTargets = acquisitionSystem.GetAvailableTargets().ToArray();
+
+            return availableTargets.OrderBy(x => Vector3.Distance(x.transform.position, transform.position)).Take(maxTargets);
+        }
+        
+        public bool IsValidTarget(GameObject target)
+        {
+            return acquisitionSystem.IsValidTarget(target);
+        }
+    }
+}
