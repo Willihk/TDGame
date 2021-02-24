f237b90c18a33d8211f2cdf19b6be348e1143f8f
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-02-24 22:04:08 +0100

    Added logic for wave ui counter

---
+++ Assets/Scripts/UI/InGame/Wave/WaveUIController.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+namespace TDGame.UI.InGame.Wave
+{
+    public class WaveUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI waveText;
+
+        public void UpdateWave(int wave)
+        {
+            waveText.text = wave.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
