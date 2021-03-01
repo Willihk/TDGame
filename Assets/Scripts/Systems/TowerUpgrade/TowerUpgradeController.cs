d5f249a7ade17e1e4f2d99c28f4313de06d44a65
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-03-01 21:10:59 +0100

    Added upgradable tower logic

---
+++ Assets/Scripts/Systems/TowerUpgrade/TowerUpgradeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Mirror;
+using TDGame.Building;
+using UnityEngine;
+
+namespace TDGame.Systems.TowerUpgrade
+{
+    public class TowerUpgradeController : NetworkBehaviour
+    {
+        public static TowerUpgradeController Instance;
+
+        [SerializeField]
+        private BuildingList prefabList;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        [Command]
+        public void TryUpgradeTower(UpgradableTower tower)
+        {
+            ReplaceTower(tower.upgradePrefab.name, tower.gameObject);
+        }
+
+        [Server]
+        public void ReplaceTower(string prefabName, GameObject oldGameObject)
+        {
+            var prefab = prefabList.GetBuilding(prefabName);
+
+            var spawned = Instantiate(gameObject);
+            spawned.transform.position = oldGameObject.transform.position;
+            spawned.transform.rotation = oldGameObject.transform.rotation;
+            
+            NetworkServer.Spawn(spawned);
+
+            NetworkServer.Destroy(oldGameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/TowerUpgrade/UpgradableTower.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace TDGame.Systems.TowerUpgrade
+{
+    public class UpgradableTower : NetworkBehaviour
+    {
+        public GameObject upgradePrefab;
+
+        public void UpgradeTower()
+        {
+            Cmd_UpgradeTower();
+        }
+
+        [Command]
+        private void Cmd_UpgradeTower()
+        {
+            TowerUpgradeController.Instance.TryUpgradeTower(this);
+        }
+    }
+}
