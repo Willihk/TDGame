2e35a16491dfe121039b2f3a40db1d2f9e6b5eff
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-03-12 15:56:15 +0100

    Added grid path script

---
+++ Assets/Scripts/Systems/Grid/InGame/GridPath.cs
@@ -0,0 +1,29 @@
+﻿using TDGame.Systems.Grid.Data;
+using UnityEngine;
+
+namespace TDGame.Systems.Grid.InGame
+{
+    public class GridPath : MonoBehaviour
+    {
+        public Transform originPoint;
+
+        public GridArea area;
+
+        private void Start()
+        {
+            var worldPosition = originPoint.position;
+
+            if (GridController.Instance)
+                area.position = GridController.Instance.towerGrid.WorldToGridPosition(worldPosition);
+        }
+        
+        void OnDrawGizmosSelected()
+        {
+            var worldPosition = originPoint.position;
+            var size = area.ConvertToWorldSize();
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(worldPosition, worldPosition + new Vector3(size.x, 0, 0));
+            Gizmos.DrawLine(worldPosition, worldPosition + new Vector3(0, 0, size.y));
+        }
+    }
+}
