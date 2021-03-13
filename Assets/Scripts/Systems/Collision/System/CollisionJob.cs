da6ab20c9919e7a48b3f020e4cdbfb09f48ed126
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-03-13 22:53:03 +0100

    Faster collision system

---
+++ Assets/Scripts/Systems/Collision/System/CollisionJob.cs
@@ -0,0 +1,38 @@
+﻿using TDGame.Systems.Collision.Collider;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace TDGame.Systems.Collision.System
+{
+    public struct CollisionResult
+    {
+        public int colliderAIndex;
+        public int colliderBIndex;
+    }
+
+    [BurstCompatible]
+    public struct CollisionJob : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<DistanceColliderData> ColliderDatas;
+
+        [WriteOnly]
+        public NativeQueue<CollisionResult>.ParallelWriter Results;
+
+        public void Execute(int index)
+        {
+            for (int otherIndex = 0; otherIndex < ColliderDatas.Length; otherIndex++)
+            {
+                if (otherIndex == index || ColliderDatas[index].CollidesWithLayer != ColliderDatas[otherIndex].Layer)
+                    continue;
+
+                float distance = math.distance(ColliderDatas[index].Center, ColliderDatas[otherIndex].Center);
+                distance = math.abs(distance);
+
+                if (distance <= ColliderDatas[index].Radius + ColliderDatas[otherIndex].Radius)
+                    Results.Enqueue(new CollisionResult {colliderAIndex = index, colliderBIndex = otherIndex});
+            }
+        }
+    }
+}
--- Assets/Scripts/Systems/Collision/System/CollisionSystem.cs
+++ Assets/Scripts/Systems/Collision/System/CollisionSystem.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using TDGame.Systems.Collision.Collider;
 using TDGame.Systems.Collision.Layer;
 using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -12,8 +13,13 @@ namespace TDGame.Systems.Collision.System
     public class CollisionSystem : MonoBehaviour
     {
         public static CollisionSystem Instance;
-        
+
         public List<DistanceCollider> Colliders;
+
+        private NativeArray<DistanceColliderData> colliderDatas;
+        private NativeQueue<CollisionResult> collisions;
+
+        private JobHandle handle;
 
         private void Awake()
         {
@@ -33,34 +39,31 @@ namespace TDGame.Systems.Collision.System
 
         private void Update()
         {
-            CheckForCollisions();
+            colliderDatas =
+                new NativeArray<DistanceColliderData>(Colliders.Select(x => x.colliderData).ToArray(),
+                    Allocator.TempJob);
+            collisions = new NativeQueue<CollisionResult>(Allocator.TempJob);
+
+            CollisionJob job = new CollisionJob()
+            {
+                ColliderDatas = colliderDatas,
+                Results = collisions.AsParallelWriter()
+            };
+            handle = job.Schedule(colliderDatas.Length, 10);
+
         }
 
-        void CheckForCollisions()
+        private void LateUpdate()
         {
-            // convert data
+            handle.Complete();
 
-            NativeArray<DistanceColliderData> colliderDatas =
-                new NativeArray<DistanceColliderData>(Colliders.Select(x => x.colliderData).ToArray(), Allocator.Temp);
-
-
-            for (int i = 0; i < colliderDatas.Length; i++)
+            while (collisions.TryDequeue(out CollisionResult collision))
             {
-                var colliderA = colliderDatas[i];
-                for (int j = 0; j < colliderDatas.Length; j++)
-                {
-                    if (j == i)
-                        continue;
-                    if (!CollidesWith(colliderA, colliderDatas[j])) 
-                        continue;
-                    
-                    Debug.Log($"{Colliders[i]} collides with {Colliders[j]}");
-
-                    Colliders[i].OnCollision(Colliders[j]);
-                    Colliders[j].OnCollision(Colliders[i]);
-                }
+                Colliders[collision.colliderAIndex].OnCollision(Colliders[collision.colliderBIndex]);
+                Colliders[collision.colliderBIndex].OnCollision(Colliders[collision.colliderBIndex]);
             }
 
+            collisions.Dispose();
             colliderDatas.Dispose();
         }
 
