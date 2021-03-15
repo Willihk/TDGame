1711097fe2bd85c42fa4b9f8a99a95dc961b806a
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-03-15 19:40:50 +0100

    Added aoe tower

---
+++ Assets/Scripts/Systems/Tower/Implementations/Heater/HeaterVFXController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TDGame.Systems.Stats;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace TDGame.Systems.Tower.Implementations.Heater
+{
+    public class HeaterVFXController : MonoBehaviour
+    {
+        [SerializeField]
+        private VisualEffect flowEffect;
+        
+        [SerializeField]
+        protected NetworkedStatsController statsController;
+        
+        [SerializeField]
+        protected string rangeStatName = "Range";
+        
+        protected StatWrapper rangeStat;
+
+        private void Start()
+        {
+            rangeStat = statsController.GetStatByName(rangeStatName);
+        }
+
+        public void Setup()
+        {
+            flowEffect.SetFloat("Range", rangeStat.stat.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/Tower/Utility/HitTimedEvents.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TDGame.Systems.Stats;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TDGame.Systems.Tower.Utility
+{
+    public class HitTimedEvents : MonoBehaviour
+    {
+        [SerializeField]
+        public UnityEvent TriggerHitEvent;
+        
+        [SerializeField]
+        private NetworkedStatsController statsController;
+
+        [SerializeField]
+        private string hitRateStatName = "HitRate";
+
+        [SerializeField]
+        private bool recurringTimer = true;
+        
+        private StatWrapper hitRateStat;
+
+        private void Awake()
+        {
+            TriggerHitEvent ??= new UnityEvent();
+        }
+
+        private void Start()
+        {
+            hitRateStat = statsController.GetStatByName(hitRateStatName);
+            StartNewEventTimer();
+        }
+
+        public void StartNewEventTimer()
+        {
+            Invoke(nameof(TriggerEvent), hitRateStat.stat.Value);
+        }
+
+        public void TriggerEvent()
+        {
+            TriggerHitEvent.Invoke();
+            if (recurringTimer)
+            {
+                StartNewEventTimer();
+            }
+        }
+    }
+}
