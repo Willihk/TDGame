20bf4c4acec3f686d6f8fa8befe778164855abd9
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-03-20 21:55:21 +0100

    Added Odin, moved Towers into Prefabs

--- Assets/Scripts/Systems/TowerUpgrade/Graph/Nodes/TowerNode.cs
+++ Assets/Scripts/Systems/TowerUpgrade/Graph/Nodes/TowerNode.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 using XNode;
 
 namespace TDGame.Systems.TowerUpgrade.Graph.Nodes
@@ -6,11 +7,17 @@ namespace TDGame.Systems.TowerUpgrade.Graph.Nodes
     [CreateNodeMenu("Tower/Upgrade")]
     public class TowerNode : Node
     {
+        [AssetsOnly]
+        [AssetSelector(Paths = "Assets/Resources/Prefabs/Towers")]
+        [Title("Tower Prefab")]
+        [HideLabel]
         public GameObject TowerPrefab;
         
+        [HorizontalGroup("Bottom")]
         [Input(ShowBackingValue.Never)]
         public Node Previous;
-
+        
+        [HorizontalGroup("Bottom")]
         [Output]
         public Node Next;
     }
