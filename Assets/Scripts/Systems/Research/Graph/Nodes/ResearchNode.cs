eda70727e777ae9006f112e1bb9775a62ef79722
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-03-20 17:03:19 +0100

    Added basic graph editor

---
+++ Assets/Scripts/Systems/Research/Graph/Nodes/ResearchNode.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace TDGame.Systems.Research.Graph.Nodes
+{
+    public class ResearchNode : Node
+    {
+        [Input(ShowBackingValue.Never)]
+        public Node Parent;
+
+        [Output]
+        public Node Children;
+
+        public GameObject prefab;
+        
+        public override object GetValue(NodePort port)
+        {
+            GetOutputPort("Children");
+            return port.fieldName == "input" ? GetInputValue(Parent.name, "None") : null;
+        }
+    }
+}
