26fb0b3dd217eaa1e94ad111049d21f0463df8d1
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-07-13 20:39:37 +0200

    Added SO events for network server/client

---
+++ Assets/Scripts/Events/Types/Network/INetworkPlayerGameEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Mirage;
+using TDGame.Events.Base;
+using UnityEngine;
+
+namespace TDGame.Events.Types.Network
+{
+    [CreateAssetMenu(fileName = "GameEvent", menuName = "GameEvents/INetworkPlayerEvent")]
+    [Serializable]
+    public class INetworkPlayerGameEvent : GameEvent<INetworkPlayer>
+    {
+    }
+}
--- Assets/Scripts/Network/Components/CustomNetworkManager.cs
+++ Assets/Scripts/Network/Components/CustomNetworkManager.cs
@@ -14,6 +14,7 @@ namespace TDGame.Network.Components
         
         [SerializeField]
         private NetworkServer server;
+        
 
         private void Awake()
         {
