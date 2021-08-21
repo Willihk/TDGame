5177f9ddd42541dd2ee9ba3feaea2299190e4d65
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-08-21 15:53:10 +0200

    map details

---
+++ Assets/Scripts/Map/Data/MapDetails.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace TDGame.Map.Data
+{
+    [CreateAssetMenu(fileName = "MapDetails", menuName = "Data/Map/MapDetails", order = 0)]
+    public class MapDetails : ScriptableObject
+    {
+        public string Name;
+
+        public string Description;
+
+        public int2 size;
+
+        [Tooltip("Reference to the scene containing the map")]
+        public AssetReference MapReference;
+    }
+}
