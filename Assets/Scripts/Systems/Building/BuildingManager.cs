46c1f1b2e6578eada947b7b79ca10700af2a0640
Will1400 <44838295+Will1400@users.noreply.github.com>
2021-10-27 20:58:15 +0200

    Basic message for spawning building

--- Assets/Scripts/Systems/Building/BuildingManager.cs
+++ Assets/Scripts/Systems/Building/BuildingManager.cs
@@ -1,13 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Cysharp.Threading.Tasks;
+using MessagePack;
+using TDGame.Network.Components.Messaging;
+using TDGame.Systems.Building.Messages.Server;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace TDGame.Systems.Building
 {
     public class BuildingManager : MonoBehaviour
     {
-        public void BuildTower(AssetReference assetReference, Vector3 position)
+        
+        
+        private BaseMessagingManager messagingManager;
+        
+        private void Start()
         {
+            messagingManager = BaseMessagingManager.Instance;
             
+            messagingManager.RegisterNamedMessageHandler<NewBuildingMessage>(Handle_NewBuildingMessage);
+        }
+
+        private async UniTask BuildTower(AssetReference assetReference, Vector3 position)
+        {
+            var handle = Addressables.InstantiateAsync(assetReference);
+
+            var spawned = await handle;
+
+            spawned.transform.position = position;
+        }
+
+        private void Handle_NewBuildingMessage(NetworkConnection sender, Stream stream)
+        {
+            var message = MessagePackSerializer.Deserialize<NewBuildingMessage>(stream);
+
+            var assetReference = new AssetReference(message.AssetGuid);
+            
+            BuildTower(assetReference, message.Position).Forget();
         }
     }
 }
---
+++ Assets/Scripts/Systems/Building/Messages/Server/NewBuildingMessage.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+using UnityEngine;
+
+namespace TDGame.Systems.Building.Messages.Server
+{
+    /// <summary>
+    /// Sent from Server to Client
+    /// </summary>
+    [MessagePackObject]
+    public struct NewBuildingMessage
+    {
+        [Key(0)]
+        public string AssetGuid;
+        
+        [Key(1)]
+        public Vector3 Position;
+    }
+}
