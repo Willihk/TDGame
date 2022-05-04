e8bff988a43cd5ee34530bcea9b90fb024749c2a
Will1400 <44838295+Will1400@users.noreply.github.com>
2022-05-04 21:53:37 +0200

    Fix buffers

---
+++ Assets/Scripts/Systems/Tower/Targeting/Systems/AddTargetBufferToTowersSystem.cs
@@ -0,0 +1,29 @@
+﻿using TDGame.Systems.Tower.Targeting.Components;
+using Unity.Entities;
+using Unity.Burst;
+
+namespace TDGame.Systems.Tower.Targeting.Systems
+{
+    public partial class AddTargetBufferToTowersSystem : SystemBase
+    {
+
+        private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+        protected override void OnCreate()
+        {
+            commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var CommandBuffer = commandBufferSystem.CreateCommandBuffer().AsParallelWriter();
+            Entities.WithAll<TowerTag, RequestEnemyTargetTag>().WithNone<TargetBufferElement>().ForEach((Entity entity, int entityInQueryIndex) =>
+            {
+                CommandBuffer.AddBuffer<TargetBufferElement>(entityInQueryIndex, entity);
+
+            }).ScheduleParallel();
+
+            commandBufferSystem.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
