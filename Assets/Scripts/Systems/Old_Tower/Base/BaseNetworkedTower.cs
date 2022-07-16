8ff1c9579af204e37ebd4b0714eb64559b6f6da6
Will1400 <44838295+Will1400@users.noreply.github.com>
2022-07-16 22:56:24 +0200

    Updated unity

--- Assets/Scripts/Systems/Old_Tower/Base/BaseNetworkedTower.cs
+++ Assets/Scripts/Systems/Old_Tower/Base/BaseNetworkedTower.cs
@@ -8,7 +8,7 @@ namespace TDGame.Systems.Tower.Base
     [Obsolete]
     public class BaseNetworkedTower : NetworkBehaviour
     {
-        public DisplayInfo DisplayInfo;
+        public TDGame.Display.Data.DisplayInfo DisplayInfo;
         
         [Header("Base")]
         public int price = 40;
---
+++ Assets/Scripts/UI/PanelSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TDGame.UI
+{
+    public class PanelSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] panels;
+
+        public void SwitchPanel(string panelName)
+        {
+            foreach (var panel in panels)
+            {
+                panel.SetActive(panel.name == panelName);
+            }
+        }
+        public void SwitchPanel(GameObject target)
+        {
+            SwitchPanel(target.name);
+        }
+    }
+}
