a4c077003d451b543b5625e9de7161c739c0c827
williamhk <williamhk@mailo.com>
2023-05-12 22:13:52 +0200

    Wave manager

---
+++ Assets/Scripts/Systems/Wave/WaveManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Hash128 = Unity.Entities.Hash128;
+
+namespace TDGame.Systems.Wave
+{
+
+    public struct WaveSpawnEntry
+    {
+        public Hash128 Guid;
+        public float Time;
+    }
+    
+    public struct WaveData
+    {
+        public WaveSpawnEntry[] SpawnEntries;
+    }
+    
+    public class WaveManager : MonoBehaviour
+    {
+        public static WaveManager Instance;
+
+        public int currentWave = 0;
+
+        private void Start()
+        {
+            Instance = this;
+        }
+
+
+        public WaveData GenerateWave()
+        {
+            return new WaveData();
+        }
+        
+    }
+}
