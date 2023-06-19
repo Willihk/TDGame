32090394a25bfcd6e4b647e081aa5ac89b9384cc
williamhk <williamhk@mailo.com>
2023-06-19 16:54:49 +0200

    Firing point

---
+++ Assets/Scripts/Systems/Tower/Targeting/Components/TurnTowardsTargetAuthoring.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace TDGame.Systems.Tower.Targeting.Components
+{
+    public struct TurnTowardsTarget : IComponentData
+    {
+        public Entity TurnPoint;
+        public float TurnSpeed;
+    }
+
+    public class TurnTowardsTargetAuthoring : MonoBehaviour
+    {
+        public GameObject TurnPoint;
+        public float TurnSpeed;
+
+        public class TurnTowardsTargetBaker : Baker<TurnTowardsTargetAuthoring>
+        {
+            public override void Bake(TurnTowardsTargetAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity,
+                    new TurnTowardsTarget
+                        {
+                            TurnPoint = GetEntity(authoring.TurnPoint, TransformUsageFlags.Dynamic),
+                            TurnSpeed = authoring.TurnSpeed
+                        });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/Tower/Targeting/Systems/TurnTowardsTargetSystem.cs
@@ -0,0 +1,41 @@
+﻿using TDGame.Systems.Tower.Targeting.Components;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace TDGame.Systems.Tower.Targeting.Systems
+{
+    public partial struct TurnTowardsTargetSystem : ISystem
+    {
+        private ComponentLookup<LocalTransform> transformLookup;
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            transformLookup = state.GetComponentLookup<LocalTransform>(true);
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            transformLookup.Update(ref state);
+
+            foreach (var (transform, turnTowardsTarget, targetBuffer) in SystemAPI
+                         .Query<RefRO<LocalTransform>, RefRO<TurnTowardsTarget>, DynamicBuffer<TargetBufferElement>>())
+            {
+                if (targetBuffer.Length <= 0)
+                    continue;
+                
+                var direction = transformLookup[targetBuffer[0]].Position - transform.ValueRO.Position;
+                direction.y = 0;
+
+                var newTransform = transformLookup[turnTowardsTarget.ValueRO.TurnPoint].WithRotation(math.nlerp(
+                    transformLookup[turnTowardsTarget.ValueRO.TurnPoint].Rotation, quaternion.LookRotation(direction, math.up()),
+                    SystemAPI.Time.DeltaTime * turnTowardsTarget.ValueRO.TurnSpeed));
+                
+                SystemAPI.SetComponent(turnTowardsTarget.ValueRO.TurnPoint, newTransform);
+            }
+        }
+    }
+}
