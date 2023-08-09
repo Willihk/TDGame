a756df7f740d8268d038393af69b8f50b95b6926
williamhk <williamhk@mailo.com>
2023-08-09 21:47:04 +0200

    format

--- Assets/Scripts/Systems/Tower/Authoring/TowerAuthoring.cs
+++ Assets/Scripts/Systems/Tower/Authoring/TowerAuthoring.cs
@@ -19,79 +19,98 @@ namespace TDGame.Systems.Tower.Authoring
             Projectile,
             Aoe
         }
-        
+
         [EnumToggleButtons]
         public TowerType towerType;
- 
+
         [BoxGroup]
         public int range;
+
         [BoxGroup]
         public float aps;
 
-        
+
         [AssetsOnly]
         [BoxGroup("Projectile", VisibleIf = "@this.towerType == TowerType.Projectile")]
         public GameObject ProjectilePrefab;
+
         [BoxGroup("Projectile")]
         public Transform FiringPoint;
 
 
         [InfoBox("Leave empty if not in use")]
-        [BoxGroup("Turn To Target",  VisibleIf = "@this.towerType == TowerType.Projectile")]
+        [BoxGroup("Turn To Target", VisibleIf = "@this.towerType == TowerType.Projectile")]
         public Transform TurnPoint;
+
         [BoxGroup("Turn To Target")]
         public float TurnSpeed;
-        
+
         [BoxGroup("AoE Damage", VisibleIf = "@this.towerType == TowerType.Aoe")]
         public bool enableAoeDamage;
 
         [BoxGroup("AoE Damage")]
         public int areaDamage;
-        
-        
-        [BoxGroup("AoE Buff",  VisibleIf = "@this.towerType == TowerType.Aoe")]
+
+
+        [BoxGroup("AoE Buff", VisibleIf = "@this.towerType == TowerType.Aoe")]
         public bool enableAoeBuff;
 
         [BoxGroup("AoE Buff/Speed")]
         public bool EnableSpeedBuff;
+
         [BoxGroup("AoE Buff/Speed"), EnableIf("EnableSpeedBuff")]
         public MovementSpeedBuff SpeedBuff;
-        
-        
+
+
         class Baker : Baker<TowerAuthoring>
         {
             public override void Bake(TowerAuthoring authoring)
             {
                 var towerEntity = GetEntity(TransformUsageFlags.Dynamic);
                 AddComponent<TowerTag>(towerEntity);
-                
-                AddComponent(towerEntity, new BaseRangeStat {Value = authoring.range});
-                AddComponent(towerEntity, new BasicWindup {Remainingtime = 1 / authoring.aps, WindupTime =1 / authoring.aps});
+
+                AddComponent(towerEntity, new BaseRangeStat { Value = authoring.range });
+                AddComponent(towerEntity,
+                    new BasicWindup { Remainingtime = 1 / authoring.aps, WindupTime = 1 / authoring.aps });
 
                 switch (authoring.towerType)
                 {
                     case TowerType.Projectile:
-                        AddComponent(towerEntity, new RequestEnemyTargets {Count = 1});
-                        
-                        AddComponent(towerEntity, new ProjectileFiringPoint {firingPoint = GetEntity(authoring.FiringPoint, TransformUsageFlags.Dynamic)});
-                        AddComponent(towerEntity, new ProjectilePrefab {Value = GetEntity(authoring.ProjectilePrefab, TransformUsageFlags.Dynamic)});
+                        AddComponent(towerEntity, new RequestEnemyTargets { Count = 1 });
+
+                        AddComponent(towerEntity,
+                            new ProjectileFiringPoint
+                                { firingPoint = GetEntity(authoring.FiringPoint, TransformUsageFlags.Dynamic) });
+                        AddComponent(towerEntity,
+                            new ProjectilePrefab
+                                { Value = GetEntity(authoring.ProjectilePrefab, TransformUsageFlags.Dynamic) });
 
                         if (authoring.TurnPoint)
                         {
-                            AddComponent(towerEntity, new TurnTowardsTarget {TurnPoint = GetEntity(authoring.TurnPoint, TransformUsageFlags.Dynamic), TurnSpeed = authoring.TurnSpeed});
+                            AddComponent(towerEntity,
+                                new TurnTowardsTarget
+                                {
+                                    TurnPoint = GetEntity(authoring.TurnPoint, TransformUsageFlags.Dynamic),
+                                    TurnSpeed = authoring.TurnSpeed
+                                });
                         }
+
                         break;
                     case TowerType.Aoe:
                         AddComponent<AoETowerTag>(towerEntity);
                         if (authoring.enableAoeDamage)
                         {
-                            AddComponent(towerEntity, new BaseDamageStat {value = authoring.areaDamage});
+                            AddComponent(towerEntity, new BaseDamageStat { value = authoring.areaDamage });
                         }
 
                         if (authoring.enableAoeBuff)
                         {
-                            AddComponent(towerEntity, authoring.SpeedBuff);
+                            if (authoring.EnableSpeedBuff)
+                            {
+                                AddComponent(towerEntity, authoring.SpeedBuff);
+                            }
                         }
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
